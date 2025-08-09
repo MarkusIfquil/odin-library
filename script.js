0987e041abdc071a62f58273ce08ee84c70c23f7
Mark Nesovanovic <mark.nesovanovic@gmail.com>
2025-08-09 17:34:01 +0200

    added card functionality, able to add books, remove books

--- script.js
+++ script.js
@@ -1,28 +1,73 @@
-const myLibrary = [];
-
 function Book(title, author, pages, read) {
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary(title,author,pages,read) {
-  let book = new Book(title,author,pages,read);
-  myLibrary.push(book);
+function addBook(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
 }
 
-function addCard() {
+function removeBook(id) {
+    for (const book of myLibrary) {
+        if (book.id == id) {
+            myLibrary.splice(myLibrary.indexOf(book), 1);
+        }
+    }
 }
+
+function createChildAndAppendIt(className, content, parent) {
+    let child = document.createElement('div');
+    child.textContent = content;
+    child.className = className;
+    parent.appendChild(child);
+}
+
+function displayBooks() {
+    let cardContainer = document.querySelector('.card-container');
+    cardContainer.innerHTML = '';
+    for (const book of myLibrary) {
+        let card = document.createElement('div');
+        card.className = 'card';
+
+        createChildAndAppendIt('title', book.title, card);
+        createChildAndAppendIt('author', book.author, card);
+        createChildAndAppendIt('pages', book.pages, card);
+        createChildAndAppendIt('read', book.read, card);
+
+        let removeButton = document.createElement('button');
+        removeButton.className = 'removeButton';
+        removeButton.textContent = 'remove book';
+        removeButton.addEventListener('click', () => { removeBook(book.id); displayBooks(); });
+        card.appendChild(removeButton);
+
+        console.log(card);
+        cardContainer.appendChild(card);
+    }
+}
+
+function submitClick(event) {
+    event.preventDefault();
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const read = document.querySelector('#read').checked;
+
+    addBook(title, author, pages, read);
+    dialog.close();
+    displayBooks();
+}
+
+const myLibrary = [];
 
 let dialog = document.querySelector("dialog");
 let addBookButton = document.querySelector('#addBook');
+let submitButton = document.querySelector('#submitButton');
 
-console.log(addBookButton);
+addBookButton.addEventListener('click', () => dialog.showModal());
+submitButton.addEventListener('click', submitClick);
 
-addBookButton.addEventListener('click',
-    () => dialog.showModal()
-);
-
-
+displayBooks();
